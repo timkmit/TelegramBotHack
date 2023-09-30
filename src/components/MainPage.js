2c5e390d2e3cea443ba1ac956b6de34c8cca9df1
Timur <timengalytchev@mail.ru>
2023-09-30 11:45:25 +0300

    add some func like id user and post request to server

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,4 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
 
 const token = '6333023640:AAFfQ8H1uYOrP-q7maIgfiFzulSTnkTQ08s'; // Замените на ваш токен
 const bot = new TelegramBot(token, { polling: true });
@@ -10,13 +11,13 @@ bot.onText(/\/start/, (msg) => {
   const options = {
     reply_markup: {
       keyboard: [
-        ['Зарегистироваться в TeamFlame'],
+        ['Ввести данные'],
       ],
       resize_keyboard: true,
     },
   };
 
-  bot.sendMessage(chatId, 'Для начала работы, выберите "Зарегистироваться в TeamFlame":', options);
+  bot.sendMessage(chatId, 'Для начала, выберите "Ввести данные":', options);
 });
 
 bot.onText(/Ввести данные/, (msg) => {
@@ -27,15 +28,17 @@ bot.onText(/Ввести данные/, (msg) => {
 });
 
 bot.on('message', (msg) => {
+  const userId = msg.from.id;
   const chatId = msg.chat.id;
   const messageText = msg.text;
+  
 
   if (userState[chatId] && userState[chatId].stage === 'name') {
-    userState[chatId].name = messageText;
+    userState[chatId].first_name = messageText; // Изменили имя
     bot.sendMessage(chatId, 'Введите вашу фамилию:');
     userState[chatId].stage = 'surname';
   } else if (userState[chatId] && userState[chatId].stage === 'surname') {
-    userState[chatId].surname = messageText;
+    userState[chatId].last_name = messageText; // Изменили фамилию
     bot.sendMessage(chatId, 'Введите вашу почту:');
     userState[chatId].stage = 'email';
   } else if (userState[chatId] && userState[chatId].stage === 'email') {
@@ -48,7 +51,15 @@ bot.on('message', (msg) => {
       bot.sendMessage(chatId, 'Повторите пароль для подтверждения:');
     } else if (messageText === userState[chatId].password) {
       const userData = userState[chatId];
-      bot.sendMessage(chatId, `Вы ввели следующие данные:\nИмя: ${userData.name}\nФамилия: ${userData.surname}\nПочта: ${userData.email}\nПароль: ${userData.password}`);
+      bot.sendMessage(chatId, `Вы ввели следующие данные:\nИмя: ${userData.first_name}\nФамилия: ${userData.last_name}\nПочта: ${userData.email}\nПароль: ${userData.password}`);
+      
+      // Выполните POST-запрос для регистрации пользователя
+      registerUser(userData, chatId);
+
+      // Отправляем сообщение с новым меню кнопок
+      sendMenu(chatId);
+      bot.sendMessage(chatId, `ID пользователя: ${userId}\nВы ввели следующие данные:\nИмя: ${userData.first_name}\nФамилия: ${userData.last_name}\nПочта: ${userData.email}\nПароль: ${userData.password}`);
+
       delete userState[chatId]; // Сбрасываем состояние пользователя
     } else {
       bot.sendMessage(chatId, 'Пароли не совпадают. Попробуйте снова. Введите ваш пароль:');
@@ -56,3 +67,56 @@ bot.on('message', (msg) => {
     }
   }
 });
+
+// Функция для отправки меню с кнопками
+function sendMenu(chatId) {
+    
+  const menuOptions = {
+    reply_markup: {
+      keyboard: [
+        ['Мое пространство'],
+        ['Создать пространство'],
+        ['Удалить пространство'],
+      ],
+      resize_keyboard: true,
+    },
+  };
+
+  bot.sendMessage(chatId, 'Выберите действие:', menuOptions);
+}
+
+// Обработка нажатия на кнопки меню
+bot.onText(/Мое пространство/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Вы выбрали "Мое пространство"');
+});
+
+bot.onText(/Создать пространство/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Вы выбрали "Создать пространство"');
+});
+
+bot.onText(/Удалить пространство/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Вы выбрали "Удалить пространство"');
+});
+
+// Функция для выполнения POST-запроса на регистрацию пользователя
+function registerUser(userData, chatId) {
+  // URL сервера для регистрации
+  const signupUrl = 'http://26.177.173.160:8888/auth/signup';
+  
+
+  axios
+    .post(signupUrl, userData)
+    .then((response) => {
+      // Обработка успешной регистрации
+      console.log('Пользователь успешно зарегистрирован:', response.data);
+       bot.sendMessage(chatId, `${JSON.stringify(response.data.message)}`);
+      
+    })
+    .catch((error) => {
+      // Обработка ошибки регистрации
+      console.error('Ошибка при регистрации пользователя:', error);
+    });
+}
